91f1254a1e2b8356b683e3faabaf10498e21dcff
Wes Dollar <wes+github@dollardojo.tech>
2023-04-06 22:00:53 -0400

    feat: persisting chat data

--- src/helpers/messages/get-chat-message/get-chat-message.ts
+++ src/helpers/messages/get-chat-message/get-chat-message.ts
@@ -10,8 +10,21 @@ export const getChatMessage = (
   message: string
 ): ChatMessageObject | undefined => {
   try {
-    // TODO: why are we match on this?
-    // 20-35/emotesv2_c086a52d3c304d88becb97389b451f76:37-51/emotesv2_e349077a987e42beaad19404fc701657:53-64/emotesv2_36f9c9d076334989a85e1b45ff430dff:0-18;first-msg=0;flags=;id=8295d1e2-fbfa-4e93-b7c5-b4c0f47f25c9;mod=0;returning-chatter=0;room-id=889699487;subscriber=1;tmi-sent-ts=1680725669243;turbo=0;user-id=889699487;user-type= :dollardojo
+    const regex2 = /user-type=\s*:(.+?)!.*\sPRIVMSG\s#dollardojo\s:(.*)/;
+    const match2 = message.match(regex2);
+
+    if (match2) {
+      // @ts-ignore go away, TS
+      const [, username, messageText] = match2;
+
+      logIncomingMessageTitle("chat message received");
+
+      return {
+        user: username as string,
+        message: formatMessageContent(messageText),
+      };
+    }
+
     const regex = /:(.+?)!(.*?)\sPRIVMSG\s#dollardojo\s:(.*)/;
     const match = message.match(regex);
 
